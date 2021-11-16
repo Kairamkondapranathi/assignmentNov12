edd45a58afadfdd064f14a2bf34268c488b2b3fe
Pranathi <89980698+Kairamkondapranathi@users.noreply.github.com>
2021-11-16 14:05:26 +0530

    Nov12

---
+++ RecursionOfFibonacci.java
@@ -0,0 +1,14 @@
+package recursion;
+
+public class RecursionOfFibonacci {
+     static int fib(int n) {
+    	 if(n<=1) return n;
+    		
+    	 return fib(n-1)+fib(n-2);
+      }
+
+     public static void main(String[] args) {
+    	
+    	 System.out.println(fib(8));
+    	 }
+}
